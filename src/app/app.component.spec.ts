47b685c0ba155a09f4c0c5b32433b59cb5748ae4
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-27 20:32:10 -0500

    se añadio Jest y pruebas unitarias

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -6,7 +8,9 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule
       ],
       declarations: [
         AppComponent
@@ -16,20 +20,72 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'appLoginTest'`, () => {
+  it(`should have as title 'AppLoginTest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('appLoginTest');
+    const component = fixture.componentInstance;
+    expect(component.title).toEqual('AppLoginTest');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('appLoginTest app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain('AppLoginTest app is running!');
+  });
+
+  //La siguiente prueba tomara una captura del código, si mas adelante alguien modificara el codigo la prueba arrojaría un error mostrando el cambio que se realizo
+  it('Se espera hacer match con el snapshot', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges(); //para capturar el texto de las variables y no solo la estructura html
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled).toMatchSnapshot(); //esto creara una copia del app.component.html en una carpeta y lo compara con el código actual
+  })
+
+  it('Se esperaba formLogin.valid retorne verdadero cuando los valores de username y password sean correcto', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    app.formLogin.controls['username'].setValue('emerson@gmail.com');
+    app.formLogin.controls['password'].setValue('12345678');
+    expect(app.formLogin.valid).toBeTruthy();
+    });
+    
+  it('Se esperaba formLogin.invalid retorne verdadero cuando los valores de username o password sean incorrecto', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+    app.formLogin.controls['username'].setValue('emerson');
+    app.formLogin.controls['password'].setValue('');
+    expect(app.formLogin.invalid).toBeTruthy();
+  });
+
+  it('Se esperaba formLogin.invalid al inicio retorne falso', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+
+    app.formLogin.controls['username'];
+    app.formLogin.controls['password'];
+    expect(app.formLogin.invalid).toBeTruthy();
+    });
+
+    it('Se espera que la variable correcto sea true al pulsar el boton y luego de 3 segundos false', () => {
+      jest.useFakeTimers();
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+      fixture.detectChanges();
+      component.formLogin.controls['username'].setValue('emerson@gmail.com');
+      component.formLogin.controls['password'].setValue('12345678');
+      const miBoton = fixture.debugElement.query(By.css('.btn-enviar'));
+      
+      miBoton.nativeElement.click(); //si se ingresa los input correctamente ejecutamos el click en el button
+      expect(component.correcto == true); //validamos que la variable correcto sea verdadero antes de los 3 segundos
+      jest.advanceTimersByTime(3000); //simulamos el setTimeout que está avanzando el tiempo en 3 segundos
+      expect(component.correcto).toBeFalsy(); //validamos que la variable correcto sea falso antes de los 3 segundos
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appLoginTest';
+export class AppComponent implements OnInit{
+  title = 'AppLoginTest';
+  correcto : boolean = false;
+  public formLogin!: FormGroup;
+
+  constructor(private formBuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(4)
+      ]],
+      
+    });
+  }
+
+  send(): any{
+    console.log(this.formLogin.value);
+
+    //Después de 3 Segundos, Reinicia el Form
+    setTimeout(() => {
+      this.correcto = false;
+      this.formLogin.reset();
+    }, 3000);
+
+    this.correcto = true;
+  }
 }
